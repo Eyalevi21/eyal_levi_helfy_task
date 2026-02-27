e6799487a9ff22e95c74fc3e0f0f6da7e655d939
Eyalevi21 <www53921@gmail.com>
2026-02-27 10:29:27 +0200

    server setup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 4000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/api/health', (req, res) => {
+    res.status(200).json({ message: 'Server is running!' });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
